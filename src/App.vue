2bd30d9bd5fb960af52a60b80eee267841106a61
BrunoSantos <101848106+Br2n0@users.noreply.github.com>
2025-04-29 10:39:45 -0300

    implementação certa

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavSideBar from './layout/NavSideBar.vue'
+import HeadBar from './layout/HeadBar.vue'
+import FooterBar from './layout/FooterBar.vue'
+import BreadCrumb from './layout/breadcrumb.vue'
+</script>
+
+<template>
+  <NavSideBar />
+  <HeadBar />
+
+  <div class="pc-container">
+    <div class="pc-content">
+
+      <BreadCrumb />
+
+      <RouterView />
+
+    </div>
+  </div>
+
+
+  <FooterBar />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './assets/fonts/tabler-icons.min.css'
+import './assets/fonts/feather.css'
+import './assets/fonts/fontawesome.css'
+import './assets/fonts/material.css'
+import './assets/css/style-preset.css'
+import './assets/css/style.css'
+
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
